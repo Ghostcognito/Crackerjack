9a97fa3bfdfe37bc78732e269b77b3cfe9d43035
ghost <ghostcognito@gmail.com>
2016-01-22 13:47:18 +0000

    Made a method to test the HMACs

--- CMAC_Maker_and_cracker.py
+++ CMAC_Maker_and_cracker.py
@@ -4,7 +4,6 @@ import random
 
 def randomLetters(lengthOfLetters):
     """This is how the random alphanumeric characters are generated"""
-    word = 'realign singularity polishing buffers'
     alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q',
             'r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8',
             '9','0','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
@@ -131,6 +130,3 @@ def crackerJack(x):
         if y == hmac2:
             listOfKeys.append(i)
     return(listOfKeys)
-
-
-        
